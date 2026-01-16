20c80888ea1ba1e57eb935cd0121c89883adb0d9
kevinpetersavage <kevinpetersavage@users.noreply.github.com>
2026-01-16 09:48:48 +0000

    Change pagination to limit by changable entry size rather than unknown size page count

--- cgpclient/client.py
+++ cgpclient/client.py
@@ -636,10 +636,11 @@ class CGPClient:
             client=self,
         )
 
-    def get_files(self) -> CGPFiles:
+    def get_files(self, max_results=100) -> CGPFiles:
         return CGPFiles(
             document_references=self.fhir_service.search_for_document_references(
-                search_params=self.fhir_config
+                search_params=self.fhir_config,
+                max_results=max_results
             ),
             client=self,
         )
